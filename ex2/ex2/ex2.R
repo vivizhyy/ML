cb5525a32c518e4fd4e1f7e1615741149983533f
yyzhang <vivizhyy@gmail.com>
2012-10-08 19:42:15 +0800

    new

--- ex2/ex2/ex2.R
+++ ex2/ex2/ex2.R
@@ -19,6 +19,8 @@ qplot(score1,score2, data=data2.table1, color=label, xlab="Exam 1 score", ylab="
 
 #  Add intercept term to x and X_test
 X <- as.matrix(cbind(data2.table1[, c(1:2)], 1))
+y <- as.matrix(data2.table1[,3])
+m <- length(y)
 
 #  sigmoid function
 sigmoid <- function(x) {
@@ -26,9 +28,28 @@ sigmoid <- function(x) {
 }
 
 #  Initialize fitting parameters
-initial_theta <- as.matrix(rep.int(0, length(names(data2.table1))))
+initial.theta <- as.matrix(rep.int(0, length(names(data2.table1))))
+
+hypothesis <- function(x, theta) {
+  return(sigmoid(x %*% theta))
+}
+
+Jcost <- function(m, theta, x, y) {
+  i <- 1
+  cost <- 0;
+  while (i <= m) {
+    cost <- cost + y[i] * log(hypothesis(X[i,], theta)) + (1 - y[i]) * log(1 - hypothesis(X[i,], theta))
+    i <- i + 1;
+  }
+  cost <- (-1/m) * cost
+  
+  return(cost)
+}
 
 #  Compute and display initial cost and gradient
-result <- function(initial_theta, X, y) {
-  
+result <- function(theta, X, y) {
+  result.list <- as.list(cost=Jcost(m, theta, X, y), grad=theta)
+  sprintf("Cost at initial theta (zeros): %f\n", as.double(result.list[1]))
+  sprintf("Gradient at initial theta (zeros): \n%f\n", result.list[c(2:4)])
+  return(result.list)
 }
